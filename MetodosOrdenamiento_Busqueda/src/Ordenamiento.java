fc4efd12ceb391333b6739a56b4d5023563af6dd
Daniel Espana <vivalarazad@gmail.com>
2018-11-23 13:07:06 -0600

    metodo burbuja agregado(burbuja1,2,3)

--- MetodosOrdenamiento_Busqueda/src/Ordenamiento.java
+++ MetodosOrdenamiento_Busqueda/src/Ordenamiento.java
@@ -1,4 +1,112 @@
 
 public class Ordenamiento {
-
+	public void burbuja1(int array[]) {
+		long tiempoInicial;
+		long tiempoFinal;
+		int pasadas=0;
+		int comparaciones=0;
+		int intercambios=0;
+		int i=0;
+		int j=1;
+		int temp=0;
+		tiempoInicial=System.currentTimeMillis();
+		for(i=0;i<array.length;i++) {
+			for(j=i+1;j<array.length;j++) {
+				comparaciones++;
+				if(array[i]>array[j]) {
+					intercambios++;
+					temp=array[i];
+					array[i]=array[j];
+					array[j]=temp;
+				}
+				pasadas++;
+			}
+			
+		}
+		tiempoFinal=System.currentTimeMillis()-tiempoInicial;
+		
+		mostrarArray(array);
+		System.out.println("Cantidad Pasadas:"+pasadas);
+		System.out.println("Cantidad Intercambios:"+intercambios);
+		System.out.println("Cantidad comparaciones:"+comparaciones);
+		System.out.println("Tiempo Ejecucion:"+tiempoFinal+" ms ");
+		
+	}
+	
+	public void burbuja2(int array[]) {
+		long tiempoInicial;
+		long tiempoFinal;
+		int pasadas=0;int comparaciones=0;
+		int intercambios=0;
+		int i=0;
+		int j=1;
+		int temp=0;
+		tiempoInicial=System.currentTimeMillis();
+		for(i=1;i<array.length;i++) {
+			comparaciones++;
+			for(j=0;j<array.length-1;j++) {
+				if(array[j]>array[j+1]) {
+					intercambios++;
+					temp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
+				}
+				pasadas++;
+			}
+		}
+		tiempoFinal=System.currentTimeMillis()-tiempoInicial;
+		
+		mostrarArray(array);
+		System.out.println("Cantidad Pasadas:"+pasadas);
+		System.out.println("Cantidad Intercambios:"+intercambios);
+		System.out.println("Cantidad comparaciones:"+comparaciones);
+		System.out.println("Tiempo Ejecucion:"+tiempoFinal+" ms ");
+	}
+	
+	
+	public void burbuja3(int array[]) 
+    { 
+		long tiempoInicial;
+		long tiempoFinal;
+		int pasadas=0;int comparaciones=0;
+		int intercambios=0;
+		int n=array.length;
+        int i, j, temp; 
+        boolean cambio;
+        tiempoInicial=System.currentTimeMillis();
+        for (i = 0; i < n - 1; i++)  
+        { 
+            cambio = false; 
+            for (j = 0; j < n - i - 1; j++)  
+            { comparaciones++;
+                if (array[j] > array[j + 1])  
+                { 
+                  intercambios++;  
+                    temp = array[j]; 
+                    array[j] = array[j + 1]; 
+                    array[j + 1] = temp; 
+                    cambio = true; 
+                }
+                pasadas++;
+            } 
+  
+            
+            if (cambio == false) 
+                break; 
+        }
+        tiempoFinal=System.currentTimeMillis()-tiempoInicial;
+        mostrarArray(array);
+        System.out.println("Cantidad Pasadas:"+pasadas);
+		System.out.println("Cantidad Intercambios:"+intercambios);
+		System.out.println("Cantidad comparaciones:"+comparaciones);
+		System.out.println("Tiempo Ejecucion:"+tiempoFinal+" ms ");
+    } 
+	
+	public void mostrarArray(int array[]) {
+		int i=0;
+		for(i=0;i<array.length;i++) {
+			System.out.print("["+array[i]+"]");
+		}
+		System.out.println();
+	}
 }
--- MetodosOrdenamiento_Busqueda/src/PruebaOrdenamiento.java
+++ MetodosOrdenamiento_Busqueda/src/PruebaOrdenamiento.java
@@ -1,8 +1,130 @@
+import java.util.Scanner;
 
 public class PruebaOrdenamiento {
 
+	static void llenarArray(int[] array) {
+		for(int i=0;i<array.length;i++) {
+			array[i]=(int) (Math.random() * 100) + 1;
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int vectorA[]=new int[1000];
+		int vectorB[]=new int[10000];
+		int vectorC[]=new int[100000];
+		int vectorD[]=new int[1000000];
+		llenarArray(vectorA);
+		llenarArray(vectorB);
+		llenarArray(vectorC);
+		llenarArray(vectorD);
+		String opcion="";
+		Scanner sc= new Scanner(System.in);
+		Ordenamiento obj=new Ordenamiento();
+		do {
+System.out.println("----Seleccione Algoritmo-----\n A)Burbuja_1\nB)Burbuja_2\nC)Burbuja_3\nZ)Salir");
+ opcion =sc.nextLine().toUpperCase();
+if(opcion.equals("A")) {
+	System.out.println("eliga cantidad de numeros para la prueba de estres...\nA)1000\nB)10,000\nC)100,000\nD)Un Millon");
+	String var=sc.nextLine().toUpperCase();
+	if(var.equals("A")) {
+		int arrayAux[]=vectorA.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja1(arrayAux);
+	}
+	if(var.equals("B")) {
+		int arrayAux[]=vectorB.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja1(arrayAux);
+	}
+	if(var.equals("C")) {
+		int arrayAux[]=vectorC.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja1(arrayAux);
+	}
+	if(var.equals("D")) {
+		int arrayAux[]=vectorD.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja1(arrayAux);
+	}
+}
+else if(opcion.equals("B")) {
+	System.out.println("eliga cantidad de numeros para la prueba de estres...\nA)1000\nB)10,000\nC)100,000\nD)Un Millon");
+	String var=sc.nextLine().toUpperCase();
+	if(var.equals("A")) {
+		int arrayAux[]=vectorA.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja2(arrayAux);
+	}
+	if(var.equals("B")) {
+		int arrayAux[]=vectorB.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja2(arrayAux);
+	}
+	if(var.equals("C")) {
+		int arrayAux[]=vectorC.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja2(arrayAux);
+	}
+	if(var.equals("D")) {
+		int arrayAux[]=vectorD.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja2(arrayAux);
+	}
+	
+}
+else if(opcion.equals("C")) {
+	System.out.println("eliga cantidad de numeros para la prueba de estres...\nA)1000\nB)10,000\nC)100,000\nD)Un Millon");
+	String var=sc.nextLine().toUpperCase();
+	if(var.equals("A")) {
+		int arrayAux[]=vectorA.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja3(arrayAux);
+	}
+	if(var.equals("B")) {
+		int arrayAux[]=vectorB.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja3(arrayAux);
+	}
+	if(var.equals("C")) {
+		int arrayAux[]=vectorC.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja3(arrayAux);
+	}
+	if(var.equals("D")) {
+		int arrayAux[]=vectorD.clone();
+		System.out.println("Array desordenado:");
+		obj.mostrarArray(arrayAux);
+		System.out.println("Array ordenado:");
+		obj.burbuja3(arrayAux);
+	}
+}
+		}while(!opcion.equals("Z"));
+
+
+sc.close();
 
 	}
 
